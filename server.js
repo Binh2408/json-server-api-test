45631cc16212f3b32befe3c29a253e5587fe7702
Binh2408 <nvbinh24082002@gmail.com>
2025-07-31 15:02:57 +0700

    update

--- server.js
+++ server.js
@@ -4,11 +4,10 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 const port = process.env.PORT || 8080;
-
 server.use(middlewares);
 server.use(router);
 
-// Quan trọng: chỉ định host là 0.0.0.0
+// ⚠ Quan trọng: chỉ định host là 0.0.0.0 để Render nhận cổng
 server.listen(port, '0.0.0.0', () => {
   console.log(`JSON Server is running on port ${port}`);
 });
